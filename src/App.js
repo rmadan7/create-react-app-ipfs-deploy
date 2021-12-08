623533f6251ef6b6cc3e8b83332625b88d1258e6
Rishi Madan <rishimadan@Rishis-MBP.verizon.net>
2021-12-07 19:56:30 -0500

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <span className="Big-plus">+</span>
         <img src={reactLogo} className="React-logo" alt="react-logo" />
         <p>
-          React App deployed on IPFS through Fleek.
+          Whats up TamaleRhino! React App deployed on IPFS through Fleek.
         </p>
         <a
           className="App-link"
